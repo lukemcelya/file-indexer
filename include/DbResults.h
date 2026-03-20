bde903214be91a0d52ebabb30e7eb76aa2988556
lukemcelya <johnlukemcelya@gmail.com>
2026-03-19 23:01:47 -0500

    Created namespace for database result structures

---
+++ include/DbResults.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "Entry.h"
+
+#include <filesystem>
+#include <string>
+#include <ostream>
+
+namespace db
+{
+  namespace fs = std::filesystem;
+
+  struct Error
+  {
+    int code{};
+    std::string message;
+
+    friend std::ostream& operator<<(std::ostream& os, const Error& e)
+    {
+      return os << "SQLite error (" << e.code << "): " << e.message;
+    }
+  };
+
+  struct FindResult
+  {
+    fs::path path;
+    Entry::EntryType type;
+    std::uintmax_t size;
+  };
+
+  struct ShowIndexResult
+  {
+    std::int64_t id;
+    fs::path root;
+    fs::file_time_type createdAt;
+    fs::file_time_type lastScannedAt;
+  };
+}
